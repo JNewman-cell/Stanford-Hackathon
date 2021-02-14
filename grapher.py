29b401960fb0d4848af47b6d14302a8dbbe53e06
JNewman-cell <72842312+JNewman-cell@users.noreply.github.com>
2021-02-13 19:02:08 -0800

    Create grapher.py

---
+++ grapher.py
@@ -0,0 +1,21 @@
+import pandas as pd
+
+import tkinter as tk
+from tkinter import simpledialog
+
+ROOT = tk.Tk()
+
+ROOT.withdraw()
+# the input dialog
+
+USER_INP = simpledialog.askstring(title="Company Name",
+                                  prompt="What's ticker symbol of the company you want to look at? (AAPL, GOOG, GOOGL, TSLA, AMZN, MSFT")      
+company = USER_INP
+
+USER_INP = simpledialog.askstring(title="Start Date",
+                                  prompt="What date do you want the stock graph to start?")
+firstDate = USER_INP
+
+USER_INP = simpledialog.askstring(title="End Date",
+                                  prompt="What date do you want the stock graph to end?")
+secondDate = USER_INP
